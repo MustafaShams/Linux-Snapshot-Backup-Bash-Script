0fd13f078b720bf7e86a2913bf61bbf10b52c0fa
KhuajaShams <ksham004@ucr.edu>
2021-12-01 00:04:19 -0800

    finished array implementaiton

--- backup.sh
+++ backup.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-backups="/home/ksham004 /root/CS183/lab2"
+backups=("/home/ksham004" "/root/CS183/lab2")
 
 day=$(date)
 day="${day// /_}"
 
 archive="$(hostname -s)-$day.tgz"
 
-echo $archive
+read userbackups
 
-tar czf /mnt/backup/$archive $backups
+echo $userbackups > custombackups.txt
 
-cp /mnt/backup/$archive /root/CS183/proj_test/$archive
+backups+=( $userbackups )
 
-echo done
+for i in ${!backups[@]}; do
+
+	echo ${backups[$i]}
+done
+
+
+echo $archive >> /root/CS183_FinalProject/test.txt
+
+#tar czf /mnt/backup/$archive $backups
+
+#cp /mnt/backup/$archive /root/CS183_FinalProject/$archive
+
+#echo backup done
