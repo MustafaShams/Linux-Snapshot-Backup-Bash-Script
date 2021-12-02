198e6727592499aa3f38f9a6b14fd98b43ab354f
KhuajaShams <ksham004@ucr.edu>
2021-12-02 02:35:39 -0800

    custom crontab

--- backup.sh
+++ backup.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
+#for test only
 default_backups="/home/ksham004 /root/CS183/lab2 "
+
+real_default_backups="/home /etc /root /var /usr/local /srv /opt"
 
 day=$(date)
 day="${day// /_}"
 
 archive="$(hostname -s)-$day.tgz"
 
+#echo "Enter custom backup filename"
+#read custom_name
+
+echo "Do you want to schedule routine backups? [y/N]: "
+read cronchoice
+
+if [ "$cronchoice" = "y" ];
+then
+	crontab -l > mycron
+	echo "*/2 * * * * bash /root/CS183_FinalProject/backup.sh" >> mycron
+	crontab mycron
+fi
+
 echo "Insert custom directories to backup or press ENTER: "
 read user_backups
-
-echo $user_backups > user_backups.txt
 
 backups="$default_backups $user_backups"
 
