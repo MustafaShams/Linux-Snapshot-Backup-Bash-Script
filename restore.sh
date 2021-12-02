724d3e8ee99345e26897bcb4dc09d8b8f22c6fac
KhuajaShams <ksham004@ucr.edu>
2021-12-02 02:57:46 -0800

    basic restore functionality

--- restore.sh
+++ restore.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+cd /
 
+echo "Enter backup file to restore: "
+read backupname
+
+sudo tar -xzvf /mnt/backup/$backupname 
+
+tput setaf 2; echo "RESTORE DONE"; tput setaf 7;
+
+cd /root/CS183_FinalProject
